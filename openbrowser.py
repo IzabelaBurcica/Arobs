378227d6aadacaa6458fa400b246f4b0cb839456
IzabelaBurcica <burcica.izabela.cuza.b15@gmail.com>
2023-09-18 11:27:58 +0300

    am deschis browser-ul si am ales un videoclip aleatoriul + am ales timpul in care browser ul sa stea deschis

--- openbrowser.py
+++ openbrowser.py
@@ -8,7 +8,7 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Chrome()
 driver.get("https://www.youtube.com")
 
-# Așteptam să se încarce pagina principală a YouTube
+# Așteptam să se încarce pagina principală a YouTube ului
 time.sleep(3)
 
 # Alegerea unui videoclip aleatoriu de pe pagina principală
@@ -16,7 +16,13 @@ video_elements = driver.find_elements(By.CSS_SELECTOR, "a#video-title")
 if video_elements:
     random_video = video_elements[random.randint(0, len(video_elements) - 1)]
     random_video.click()
-    time.sleep(3)  # încărcarea paginii videoclipului
+    time.sleep(3)
+
+    # Redam videoclipul
+    player = driver.find_element(By.CSS_SELECTOR, ".ytp-play-button")
+    player.click()
+else:
+    print("Nu s-au găsit videoclipuri pe pagina principală.")
 
 # Așteptam o anumită perioadă, apoi închidem browser-ul
 time.sleep(120)  # timpul in care browser ul sta deschis
