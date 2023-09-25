d5783fe216a4b34eda917dfa9a260f9b200df417
IzabelaBurcica <burcica.izabela.cuza.b15@gmail.com>
2023-09-25 11:38:03 +0300

    testam cele 2 inregistrari

--- main.py
+++ main.py
@@ -11,10 +11,16 @@ from VideoRecorder import record_video
 driver = webdriver.Chrome()
 driver.get("https://www.youtube.com")
 
+
 # Așteptam să se încarce pagina principală a YouTube ului
 time.sleep(3)
 
-# Alegem un videoclip aleatoriu de pe pagina principală
+#pornim cele 2 inregistrari
+
+record_audio("test_audio.wav",13)
+record_video("test_video.avi",13)
+
+# Alegerea unui videoclip aleatoriu de pe pagina principală
 video_elements = driver.find_elements(By.CSS_SELECTOR, "a#video-title")
 if video_elements:
     random_video = video_elements[random.randint(0, len(video_elements) - 1)]
@@ -24,10 +30,8 @@ if video_elements:
     # Redam videoclipul
     player = driver.find_element(By.CSS_SELECTOR, ".ytp-play-button")
     player.click()
-
 else:
     print("Nu s-a gasit videoclip")
-
 # Așteptam o anumită perioadă, apoi închidem browser-ul
 time.sleep(20)  # timpul in care browser ul sta deschis
 driver.quit()
